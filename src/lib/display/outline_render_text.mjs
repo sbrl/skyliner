87a0a6fea85c35e088064795bfe718458fb4ba52
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-03-14 17:59:29 +0000

    outline_render_text: correctly handle child nodes

--- src/lib/display/outline_render_text.mjs
+++ src/lib/display/outline_render_text.mjs
@@ -1,13 +1,15 @@
 "use strict";
 
-export default function(outline, depth = 0) {
+function outline_render_text(outline, depth = 0) {
 	let result = [];
 	
 	for(let item of outline) {
 		result.push(`\t`.repeat(depth));
 		result.push(`${item.line}:${item.index} ${item.text}\n`);
 		if(item.children instanceof Array)
-			result.push(this.render(item.children, depth + 1));
+			result.push(outline_render_text(item.children, depth + 1));
 	}
 	return result.join("");
 }
+
+export default outline_render_text
