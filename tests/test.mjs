dbd4c247701fd41afd8e0e8aea820f8f5773e1bf
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-03-14 15:01:15 +0000

    test.mjs: make test runner synchronous, because ava doesn't support async test definitions :-/

--- tests/test.mjs
+++ tests/test.mjs
@@ -12,7 +12,7 @@ const __dirname = import.meta.url.slice(7, import.meta.url.lastIndexOf("/"));
 
 const skyliner = new Skyliner();
 
-async function do_test(test_name, data) {
+function do_test(test_name, data) {
 	test(test_name, async t => {
 		let outline = null, target = null;
 		await Promise.all([
@@ -23,26 +23,22 @@ async function do_test(test_name, data) {
 	});
 }
 
-(async () => {
-	for(let lang of (await fs.promises.readdir(__dirname))) {
-		let dir = path.join(__dirname, lang);
-		if(!(await fs.promises.lstat(dir)).isDirectory()) continue;
+for(let lang of fs.readdirSync(__dirname)) {
+	let dir = path.join(__dirname, lang);
+	if(!(fs.lstatSync(dir)).isDirectory()) continue;
+	
+	let files = fs.readdirSync(dir)
+		.map((el) => { let m = el.match(/([^.]+)\.(in|out)/); return [ m[1], m[2], el ]});
+	
+	let grouped = {};
+	for(let [ test_name, file_type, filename ] of files) {
+		if(typeof grouped[test_name] !== "object")
+			grouped[test_name] = { lang };
 		
-		let files = (await fs.promises.readdir(dir))
-			.map((el) => { let m = el.match(/([^.]+)\.(in|out)/); return [ m[1], m[2], el ]});
-		
-		let grouped = {};
-		for(let [ test_name, file_type, filename ] of files) {
-			if(typeof grouped[test_name] !== "object")
-				grouped[test_name] = { lang };
-			
-			grouped[test_name][file_type] = path.join(dir, filename);
-		}
-		
-		let promises = [];
-		for(let test_name in grouped) {
-			promises.push(do_test(`${lang}:${test_name}`, grouped[test_name]));
-		}
-		await Promise.all(promises);
+		grouped[test_name][file_type] = path.join(dir, filename);
 	}
-})();
+	
+	for(let test_name in grouped) {
+		do_test(`${lang}:${test_name}`, grouped[test_name]);
+	}
+}
