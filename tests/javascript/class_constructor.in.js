bbf39e2bdf21b7ecb697b6d66eef08f64fb7c655
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-03-14 02:44:30 +0000

    javascript: Add some more tests

---
+++ tests/javascript/class_constructor.in.js
@@ -0,0 +1,9 @@
+"use strict";
+
+class SomeClass {
+	constructor() {
+		
+	}
+}
+
+export default SomeClass;
---
+++ tests/javascript/class_constructor_subcall.in.js
@@ -0,0 +1,9 @@
+"use strict";
+
+class SomeClass {
+	constructor() {
+		foo();
+	}
+}
+
+export default SomeClass;
